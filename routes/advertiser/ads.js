e751d87459e490942a11cf7efcc2e54f14a6f08e
Enoch <enoch@getinnotized.com>
2021-04-28 04:51:24 +0000

    fixes casing in routes for ads

--- routes/advertiser/ads.js
+++ routes/advertiser/ads.js
@@ -1,28 +1,28 @@
 //Import Router
-const router = require('express').Router();
+const router = require("express").Router();
 
 //Import Ad Model
-const Ad = require('../../models/Ad');
+const Ad = require("../../models/ad");
 
 //Import Advertizer Schema
-const advertizerSchema = require('../../schema/advertizerSchema');
+const advertizerSchema = require("../../schema/advertizerSchema");
 
 //Import Token Functions
-const authToken = require('../../helpers/tokenfunctions');
+const authToken = require("../../helpers/tokenfunctions");
 
 //===================================================================================================================//
 //                                      {Fetch all Ads Endpoint}
 //===================================================================================================================//
-router.get("/viewallads", async (req, res)=>{
-    //
-    try {
-        //Fetch all ads
-        const fetchAds = await Ad.find();
+router.get("/viewallads", async (req, res) => {
+  //
+  try {
+    //Fetch all ads
+    const fetchAds = await Ad.find();
 
-        res.send({message: "Fecth Successful", fetchAds});
-    } catch (error) {
-        res.status(404).send({error: error.message});
-    }
+    res.send({ message: "Fecth Successful", fetchAds });
+  } catch (error) {
+    res.status(404).send({ error: error.message });
+  }
 });
 
 //===================================================================================================================//
@@ -42,64 +42,69 @@ router.get("/viewallads", async (req, res)=>{
 //===================================================================================================================//
 //                                      {Create Ad Endpoint}
 //===================================================================================================================//
-router.post("/createad", advertizerSchema.upload.single('adImage'), authToken.verifyToken, async (req, res)=>{
-   // console.log(req.file);
-   // console.log(req.body);
+router.post(
+  "/createad",
+  advertizerSchema.upload.single("adImage"),
+  authToken.verifyToken,
+  async (req, res) => {
+    // console.log(req.file);
+    // console.log(req.body);
 
     //Check if the payload is not empty
-    if(!req.body.ad_detail) return res.status(404).json({error: "Advertizser object is empty"});
+    if (!req.body.ad_detail)
+      return res.status(404).json({ error: "Advertizser object is empty" });
 
     try {
-        //Validate
-        await advertizerSchema.advertiser_Schema.validateAsync(req.body);
+      //Validate
+      await advertizerSchema.advertiser_Schema.validateAsync(req.body);
 
-        const ad_detail = req.body.ad_detail;
+      const ad_detail = req.body.ad_detail;
 
-        const ad_image_file = req.file;
+      const ad_image_file = req.file;
 
-        ad_detail.ad_image = `adImages/${ad_image_file.filename}`;
+      ad_detail.ad_image = `adImages/${ad_image_file.filename}`;
 
-        //Create Ad
-        const createAdvert = await Ad({ad_detail}).save();
+      //Create Ad
+      const createAdvert = await Ad({ ad_detail }).save();
 
-        //
-        if(!createAdvert){
-            res.send({error: "Ad Creation Failed"});
-        }else{
-            res.send({message: "successfully created", createAdvert});
-        }
+      //
+      if (!createAdvert) {
+        res.send({ error: "Ad Creation Failed" });
+      } else {
+        res.send({ message: "successfully created", createAdvert });
+      }
     } catch (error) {
-     res.status(404).send({error: error.message});   
+      res.status(404).send({ error: error.message });
     }
-});
+  }
+);
 
 //===================================================================================================================//
 //                                      {Update Ad Endpoint}
 //===================================================================================================================//
-router.patch("/updatead", async (req, res)=>{
-    //
-})
+router.patch("/updatead", async (req, res) => {
+  //
+});
 //===================================================================================================================//
 //                                      {Fetch all Ads Endpoint}
 //===================================================================================================================//
-router.get("/deletead/:ad_id", authToken.verifyToken, async (req, res)=>{
+router.get("/deletead/:ad_id", authToken.verifyToken, async (req, res) => {
+  //
+  try {
     //
-        try {
-            //
-            const _id = req.params.ad_id;
+    const _id = req.params.ad_id;
 
-            const deleteAdvert = await Ad.findOneAndDelete({_id});
-            //console.log(deleteAdvert);
-            
-            if(!deleteAdvert){
-                res.send({error: "Deletion Failed"});
-            }else{
-                res.send({message: "Ad Successfully Deleted"});
-            };
+    const deleteAdvert = await Ad.findOneAndDelete({ _id });
+    //console.log(deleteAdvert);
 
-        } catch (error) {
-            res.status(404).send({error: error.message});
-        }
-})
+    if (!deleteAdvert) {
+      res.send({ error: "Deletion Failed" });
+    } else {
+      res.send({ message: "Ad Successfully Deleted" });
+    }
+  } catch (error) {
+    res.status(404).send({ error: error.message });
+  }
+});
 
 module.exports = router;
